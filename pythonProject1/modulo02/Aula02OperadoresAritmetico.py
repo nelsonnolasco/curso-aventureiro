4276734848a557539a2690f2cecac15cbf8c6431
Nelson de Campos Nolasco <nelsonnolasco@gmail.com>
2024-12-04 23:36:47 -0300

    Modulo02 - Introdução - Operadores e Tipos de dados

---
+++ pythonProject1/modulo02/Aula02OperadoresAritmetico.py
@@ -0,0 +1,27 @@
+numero1 = 5
+numero2 = 2
+
+soma = numero1 + numero2
+
+subtracao = numero1 - numero2
+
+multiplicacao = numero1 * numero2
+
+divisao = numero1 / numero2
+
+divisaoInteira = numero1 // numero2
+
+modulo = numero1 % numero2
+
+exponenciacao = numero1 ** numero2
+
+print("soma", soma)
+print("subtracao", subtracao)
+print("multiplicacao", multiplicacao)
+print("divisao", divisao)
+print("divisaoInteira", divisaoInteira)
+print("modulo", modulo)
+print("divisão inteira", divisaoInteira)
+print("exponenciacao", exponenciacao)
+
+print("Curso de Python " * 3)
---
+++ pythonProject1/modulo02/Aula03TiposDados.py
@@ -0,0 +1,10 @@
+inteiro = 23
+realFloat = 1.23
+nomeString = "Curso de Python"
+
+print("Inteiro", inteiro)
+print("Float", realFloat)
+print("String", nomeString)
+
+
+
