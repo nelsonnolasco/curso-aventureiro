7f6c8db753167d5cefeb343476f9effd2cdd1245
Nelson de Campos Nolasco <nelsonnolasco@gmail.com>
2024-12-05 01:39:49 -0300

    Flet

--- pythonProject1/modulo02/Aula03TiposDados.py
+++ pythonProject1/modulo02/Aula03TiposDados.py
@@ -1,10 +1,12 @@
 inteiro = 23
 realFloat = 1.23
 nomeString = "Curso de Python"
+lista = [1, 2, 'tres', 4, 5]
 
 print("Inteiro", inteiro)
 print("Float", realFloat)
 print("String", nomeString)
+print("Lista", lista)
 
 
 
